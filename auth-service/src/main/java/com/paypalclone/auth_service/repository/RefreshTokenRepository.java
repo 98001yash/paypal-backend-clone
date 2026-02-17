185ad70f0087e6cd5dc242b190fff87d484be8f5
98001yash <yashchauhan.gaya@gmail.com>
2026-02-17 09:15:57 +0530

    created all the repository in the auth-service

---
+++ auth-service/src/main/java/com/paypalclone/auth_service/repository/RefreshTokenRepository.java
@@ -0,0 +1,16 @@
+package com.paypalclone.auth_service.repository;
+
+import com.paypalclone.auth_service.entity.RefreshToken;
+import com.paypalclone.auth_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.Instant;
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+
+    Optional<RefreshToken>  findByToken(String token);
+    void deleteUser(User user);
+    void deleteByExpiryDateBefore(Instant now);
+}
