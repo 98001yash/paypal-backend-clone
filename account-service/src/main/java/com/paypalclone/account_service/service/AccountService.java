a0d0ad4d310e156a77e6a2219257e7678c2debe8
98001yash <yashchauhan.gaya@gmail.com>
2026-02-19 18:16:53 +0530

    account service interface added

---
+++ account-service/src/main/java/com/paypalclone/account_service/service/AccountService.java
@@ -0,0 +1,18 @@
+package com.paypalclone.account_service.service;
+
+import com.paypalclone.account_service.entity.Account;
+import com.paypalclone.account_service.enums.AccountType;
+
+import java.util.List;
+
+public interface AccountService {
+
+
+    Account createAccount(Long ownerId, AccountType accountType);
+
+    void activateAccount(Long ownerId, AccountType accountType);
+
+    void suspendAccount(Long ownerId, AccountType accountType);
+
+    List<Account>  getAccountForOwner(Long ownerId);
+}
