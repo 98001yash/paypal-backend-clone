4b93f8efe977fbdfbacae75184e4de3fa38ae35b
98001yash <yashchauhan.gaya@gmail.com>
2026-02-20 13:06:07 +0530

    added the balance projection microserice

---
+++ balance-project-service/src/main/java/com/paypalclone/balance_project_service/BalanceProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paypalclone.balance_project_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalanceProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalanceProjectServiceApplication.class, args);
+	}
+
+}
---
+++ balance-project-service/src/test/java/com/paypalclone/balance_project_service/BalanceProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypalclone.balance_project_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalanceProjectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
