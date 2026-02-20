704d5a5b7c1d2174a7a5288a33121e1b157da543
98001yash <yashchauhan.gaya@gmail.com>
2026-02-20 09:40:46 +0530

    ledger microservice added in the system

---
+++ ledger-service/src/test/java/com/paypalclone/ledger_service/LedgerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypalclone.ledger_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
