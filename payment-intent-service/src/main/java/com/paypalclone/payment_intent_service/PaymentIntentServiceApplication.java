48bfd43745694b00d03d9815ce2ad9cfc583cde6
98001yash <yashchauhan.gaya@gmail.com>
2026-02-21 10:39:04 +0530

    added the payment intent service

---
+++ payment-intent-service/src/main/java/com/paypalclone/payment_intent_service/PaymentIntentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.paypalclone.payment_intent_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentIntentServiceApplication.class, args);
+	}
+
+}
---
+++ payment-intent-service/src/test/java/com/paypalclone/payment_intent_service/PaymentIntentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypalclone.payment_intent_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
