b0e723135c99ae802fc49ed0e5545ad22ed21589
98001yash <yashchauhan.gaya@gmail.com>
2026-02-22 07:45:16 +0530

    added the payout enums

---
+++ payout-service/src/main/java/com/paypalclone/payout_service/enums/PayoutBatchStatus.java
@@ -0,0 +1,9 @@
+package com.paypalclone.payout_service.enums;
+
+public enum PayoutBatchStatus {
+
+    CREATED,
+    SENT_TO_BANK,
+    COMPLETED,
+    FAILED
+}
---
+++ payout-service/src/main/java/com/paypalclone/payout_service/enums/PayoutStatus.java
@@ -0,0 +1,10 @@
+package com.paypalclone.payout_service.enums;
+
+public enum PayoutStatus {
+
+    CREATED,
+    QUEUED,
+    SENT_TO_BANK,
+    COMPLETED,
+    FAILED
+}
